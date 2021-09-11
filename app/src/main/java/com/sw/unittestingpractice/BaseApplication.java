c621a3678b5c6a88799d1d22d06ac2f5a5570396
SohaybHassan <sohiyp199716@gmail.com>
2021-09-11 10:53:25 +0300

    Complet  init dagger

--- app/src/main/java/com/sw/unittestingpractice/BaseApplication.java
+++ app/src/main/java/com/sw/unittestingpractice/BaseApplication.java
@@ -1,11 +1,14 @@
 package com.sw.unittestingpractice;
 
+import com.sw.unittestingpractice.di.AppComponent;
+import com.sw.unittestingpractice.di.DaggerAppComponent;
+
 import dagger.android.AndroidInjector;
 import dagger.android.DaggerApplication;
 
 public class BaseApplication extends DaggerApplication {
     @Override
     protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
-        return null;
+        return DaggerAppComponent.builder().application(this).build();
     }
 }
